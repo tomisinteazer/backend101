5570e5827265da1de37769bce0e9f3c47e7df558
tomisinteazer <tomisinteazer@gmail.com>
2021-08-17 01:07:18 +0100

    basic stuff done

---
+++ index.js
@@ -0,0 +1,61 @@
+//imports
+let express = require("express");
+const { EventEmitter } = require("events");
+let httpmod = require("http");
+
+//inits
+const eventMaker = new EventEmitter();
+let app = express();
+
+//imports and inittializations are on top
+
+app.get("/", (req, res) => {
+    res.send("home page bro");
+});
+
+function spacer() {
+    console.log(".");
+    console.log(".");
+    console.log(".");
+    console.log(".");
+    console.log(".");
+
+    console.log("done");
+}
+
+function eventtester() {
+    eventMaker.on("env", () => {
+        console.log(process.env);
+        spacer();
+    });
+
+    eventMaker.emit("env");
+}
+
+function playgroundServer() {
+    let myServer = httpmod.createServer((req, res) => {
+        if (req.url === "/") {
+            res.write("took you long enough");
+            res.end();
+        }
+        if (req.url === "/about") {
+            res.write("about blah blah  enough");
+            res.end();
+        }
+    });
+    myServer.listen(7500);
+    myServer.on("connection", () => {
+        console.log("basic server don dey run");
+    });
+    //will comeback to this later or not
+}
+function psuedomain() {
+    // app.listen(7000, () => {
+    //     console.log("someone connect5ed");
+    // });
+
+    // playgroundServer();
+    eventtester();
+}
+
+psuedomain();
